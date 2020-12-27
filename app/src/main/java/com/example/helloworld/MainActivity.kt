e1691818537c70e4c0a0ab7db842c94bc2e65f9a
Hollie Baker <hollie@localhost.localdomain>
2020-12-27 23:47:10 +0000

    Add image view
    - add image resources
    - update image and content description depending on number rolled

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +18,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val rollTextView: TextView = findViewById(R.id.text_roll_value)
+        val diceImageView: ImageView = findViewById(R.id.image_dice)
+
         val rollValue: Int = (1..6).random()
+        val rollImage = when (rollValue) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
         rollTextView.text = getString(R.string.label_rolled) + rollValue.toString()
+
+        diceImageView.setImageResource(rollImage)
+        // updating the content description for TalkBack
+        diceImageView.contentDescription = rollValue.toString()
     }
 }
